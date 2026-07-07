a8a5253e032f80436ec78f781ae1fbf6de119227
WarmiKapital <sistemas@warmikapital.com.pe>
2026-07-07 09:13:01 -0500

    feat: redirect /webmail path to external host
    Redirect requests to the /webmail path to the official webmail portal for Warmi Kapital.

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,7 @@
+if (typeof window !== 'undefined' && /^\/webmail\/?$/i.test(window.location.pathname)) {
+  window.location.replace("https://webmail.warmikapital.com.pe");
+}
+
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
